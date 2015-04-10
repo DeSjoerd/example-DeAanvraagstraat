508bb348dc35e5f26676a8678191cbad4948a511
DeSjoerd <M@DeSjoerd.com>
2015-04-10 10:45:07 +0200

    Initial commit

---
+++ DeAanvraagstraat.WebApi/App_Start/Startup.WebApi.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace DeAanvraagstraat.WebApi
+{
+    public partial class Startup
+    {
+        public void ConfigureWebApi(IAppBuilder app, ILifetimeScope container)
+        {
+            var config = new HttpConfiguration();
+
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            config.MapHttpAttributeRoutes();
+
+            // use camelCase for output and input instead of PascalCase
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            app.UseAutofacWebApi(config);
+            app.UseWebApi(config);
+        }
+    }
+}
---
+++ DeAanvraagstraat.WebApi/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using Autofac.Integration.Owin;
+using Autofac.Integration.WebApi;
+using DeAanvraagstraat.WebApi;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace DeAanvraagstraat.WebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var container = BuildContainer();
+
+            app.UseAutofacMiddleware(container);
+
+            ConfigureWebApi(app, container);
+        }
+
+        private ILifetimeScope BuildContainer()
+        {
+            var thisAssembly = typeof(Startup).Assembly;
+            var builder = new ContainerBuilder();
+            
+            builder.RegisterApiControllers(thisAssembly);
+
+            return builder.Build();
+        }
+    }
+}
